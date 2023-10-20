c3872abe32ee64e66ac868d2d1f423756128b5a5
Mauro Arantes <mauro.arantes@gmail.com>
2023-10-20 14:21:16 +0100

    Initial Commit

---
+++ TubeLinesTFL/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TubeLinesTFL
+//
+//  Created by Mauro Arantes on 20/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TubeLinesTFL/TubeLinesTFLApp.swift
@@ -0,0 +1,17 @@
+//
+//  TubeLinesTFLApp.swift
+//  TubeLinesTFL
+//
+//  Created by Mauro Arantes on 20/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct TubeLinesTFLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
